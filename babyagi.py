91c315bef9fc46d030dcf7f9c765dd70761637a9
Ely <769165+jozzua@users.noreply.github.com>
2023-04-10 21:01:40 +0800

    Just added more commenting on code

--- babyagi.py
+++ babyagi.py
@@ -9,7 +9,7 @@ from typing import Dict, List
 from dotenv import load_dotenv
 import os
 
-#Set Variables
+# Set Variables
 load_dotenv()
 
 # Set API Keys
@@ -19,7 +19,8 @@ assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env
 # Use GPT-3 model
 USE_GPT4 = False
 if USE_GPT4:
-    print("\033[91m\033[1m"+"\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"+"\033[0m\033[0m")
+    print("\033[91m\033[1m" +
+          "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"+"\033[0m\033[0m")
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
 assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable is missing from .env"
@@ -38,7 +39,7 @@ assert OBJECTIVE, "OBJECTIVE environment variable is missing from .env"
 YOUR_FIRST_TASK = os.getenv("FIRST_TASK", "")
 assert YOUR_FIRST_TASK, "FIRST_TASK environment variable is missing from .env"
 
-#Print OBJECTIVE
+# Print OBJECTIVE
 print("\033[96m\033[1m"+"\n*****OBJECTIVE*****\n"+"\033[0m\033[0m")
 print(OBJECTIVE)
 
@@ -52,7 +53,8 @@ dimension = 1536
 metric = "cosine"
 pod_type = "p1"
 if table_name not in pinecone.list_indexes():
-    pinecone.create_index(table_name, dimension=dimension, metric=metric, pod_type=pod_type)
+    pinecone.create_index(table_name, dimension=dimension,
+                          metric=metric, pod_type=pod_type)
 
 # Connect to the index
 index = pinecone.Index(table_name)
@@ -60,16 +62,19 @@ index = pinecone.Index(table_name)
 # Task list
 task_list = deque([])
 
+
 def add_task(task: Dict):
     task_list.append(task)
+
 
 def get_ada_embedding(text):
     text = text.replace("\n", " ")
     return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
 
+
 def openai_call(prompt: str, use_gpt4: bool = False, temperature: float = 0.5, max_tokens: int = 100):
     if not use_gpt4:
-        #Call GPT-3 DaVinci model
+        # Call GPT-3 DaVinci model
         response = openai.Completion.create(
             engine='text-davinci-003',
             prompt=prompt,
@@ -81,11 +86,11 @@ def openai_call(prompt: str, use_gpt4: bool = False, temperature: float = 0.5, m
         )
         return response.choices[0].text.strip()
     else:
-        #Call GPT-4 chat model
-        messages=[{"role": "user", "content": prompt}]
+        # Call GPT-4 chat model
+        messages = [{"role": "user", "content": prompt}]
         response = openai.ChatCompletion.create(
             model="gpt-4",
-            messages = messages,
+            messages=messages,
             temperature=temperature,
             max_tokens=max_tokens,
             n=1,
@@ -93,13 +98,15 @@ def openai_call(prompt: str, use_gpt4: bool = False, temperature: float = 0.5, m
         )
         return response.choices[0].message.content.strip()
 
+
 def task_creation_agent(objective: str, result: Dict, task_description: str, task_list: List[str], gpt_version: str = 'gpt-3'):
     prompt = f"You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: {objective}, The last completed task has the result: {result}. This result was based on this task description: {task_description}. These are incomplete tasks: {', '.join(task_list)}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array."
     response = openai_call(prompt, USE_GPT4)
     new_tasks = response.split('\n')
     return [{"task_name": task_name} for task_name in new_tasks]
 
-def prioritization_agent(this_task_id:int, gpt_version: str = 'gpt-3'):
+
+def prioritization_agent(this_task_id: int, gpt_version: str = 'gpt-3'):
     global task_list
     task_names = [t["task_name"] for t in task_list]
     next_task_id = int(this_task_id)+1
@@ -117,23 +124,27 @@ def prioritization_agent(this_task_id:int, gpt_version: str = 'gpt-3'):
             task_name = task_parts[1].strip()
             task_list.append({"task_id": task_id, "task_name": task_name})
 
-def execution_agent(objective:str,task: str, gpt_version: str = 'gpt-3') -> str:
-    #context = context_agent(index="quickstart", query="my_search_query", n=5)
-    context=context_agent(index=YOUR_TABLE_NAME, query=objective, n=5)
-    #print("\n*******RELEVANT CONTEXT******\n")
-    #print(context)
-    prompt =f"You are an AI who performs one task based on the following objective: {objective}.\nTake into account these previously completed tasks: {context}\nYour task: {task}\nResponse:"
+
+def execution_agent(objective: str, task: str, gpt_version: str = 'gpt-3') -> str:
+    # context = context_agent(index="quickstart", query="my_search_query", n=5)
+    context = context_agent(index=YOUR_TABLE_NAME, query=objective, n=5)
+    # print("\n*******RELEVANT CONTEXT******\n")
+    # print(context)
+    prompt = f"You are an AI who performs one task based on the following objective: {objective}.\nTake into account these previously completed tasks: {context}\nYour task: {task}\nResponse:"
     return openai_call(prompt, USE_GPT4, 0.7, 2000)
+
 
 def context_agent(query: str, index: str, n: int):
     query_embedding = get_ada_embedding(query)
     index = pinecone.Index(index_name=index)
     results = index.query(query_embedding, top_k=n,
-    include_metadata=True)
-    #print("***** RESULTS *****")
-    #print(results)
-    sorted_results = sorted(results.matches, key=lambda x: x.score, reverse=True)    
+                          include_metadata=True)
+    # print("***** RESULTS *****")
+    # print(results)
+    sorted_results = sorted(
+        results.matches, key=lambda x: x.score, reverse=True)
     return [(str(item.metadata['task'])) for item in sorted_results]
+
 
 # Add the first task
 first_task = {
@@ -157,19 +168,23 @@ while True:
         print(str(task['task_id'])+": "+task['task_name'])
 
         # Send to execution function to complete the task based on the context
-        result = execution_agent(OBJECTIVE,task["task_name"])
+        result = execution_agent(OBJECTIVE, task["task_name"])
         this_task_id = int(task["task_id"])
         print("\033[93m\033[1m"+"\n*****TASK RESULT*****\n"+"\033[0m\033[0m")
         print(result)
 
         # Step 2: Enrich result and store in Pinecone
-        enriched_result = {'data': result}  # This is where you should enrich the result if needed
+        # This is where you should enrich the result if needed
+        enriched_result = {'data': result}
         result_id = f"result_{task['task_id']}"
-        vector = enriched_result['data']  # extract the actual result from the dictionary
-        index.upsert([(result_id, get_ada_embedding(vector),{"task":task['task_name'],"result":result})])
+        # extract the actual result from the dictionary
+        vector = enriched_result['data']
+        index.upsert([(result_id, get_ada_embedding(vector), {
+                     "task": task['task_name'], "result":result})])
 
     # Step 3: Create new tasks and reprioritize task list
-    new_tasks = task_creation_agent(OBJECTIVE,enriched_result, task["task_name"], [t["task_name"] for t in task_list])
+    new_tasks = task_creation_agent(OBJECTIVE, enriched_result, task["task_name"], [
+                                    t["task_name"] for t in task_list])
 
     for new_task in new_tasks:
         task_id_counter += 1
